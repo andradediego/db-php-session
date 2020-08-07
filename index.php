e2f1e52d93afa8fb2657044aa9441b1f75c35d86
Diego Andrade <andrade.diego@outlook.com>
2020-08-07 10:14:00 -0400

    Lab goal achieved

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+	session_start();
+	
+	
+	// echo 'The session ID is:' . session_id();
+	$number_of_tries = 0;
+	if(!isset($_SESSION['user_new'])) {
+		$_SESSION['user_new'] = 0;
+		// echo 'Created Session';
+	} else {
+		$number_of_tries = $_SESSION['user_new'];
+	}
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Diego - Lab 3</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="text-center">My Favorite Movies</h2>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-12">
+				<a href="table.php" class="btn btn-primary btn-block">View All Records</a>
+			</div>
+		</div>
+		
+		<hr>
+		<div class="row">
+			<div class="col-md-12">
+				<?php 
+					if ($number_of_tries < 3) {
+				?>
+				<form action="table.php" method="post">
+					<div class="form-group">
+						<label for="name">Movie Name</label>
+						<input type="text" name="name" class="form-control" id="name" placeholder="Ex.: Highlander">
+					</div>
+					<div class="form-group">
+						<label for="rating">Rating</label>
+						<select class="form-control" name="rating" id="rating">
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+						</select>
+					</div>
+					<hr>
+					<input type="submit" class="btn btn-primary" value="Submit">
+				</form>
+				<?php 
+					} else {
+				?>
+				<p class="bg bg-danger">
+						You have reached the maximum of 3 submission for this session. Please return later.
+				</p>
+				<?php 
+					}
+				?>
+			</div>
+		</div>
+	</div>
+	<script src="js/main.js"></script>
+</body>
+</html>
---
+++ table.php
@@ -0,0 +1,88 @@
+<?php
+	$mysqli = new mysqli('localhost', 'root', '', 'db_php');
+	
+	session_start();
+	
+	$post_times = 0;
+	if(isset($_SESSION['user_new'])) {
+		$post_times = $_SESSION['user_new'];
+	}
+	
+
+	if ($post_times < 3) {
+		if (isset($_POST['name']) && isset($_POST['rating'])) {
+			$name = htmlentities($_POST['name']);
+			$rate = htmlentities($_POST['rating']);
+			
+			$insert = "INSERT INTO `movies` (`name`, `rating`) VALUES ('" . $name . "', " . $rate . ")";
+	
+			$result = mysqli_query($mysqli, $insert);
+			if($result) {
+				$_SESSION['user_new'] = $post_times + 1;
+			}
+		}
+	}
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Diego - Lab 3</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="text-center">My Favorite Movies</h2>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-12">
+			<?php
+				if (isset($_POST['name']) && isset($_POST['rating'])) {
+			?>
+				<p class="bg bg-success">
+					Your entry was successfully inserted!
+				</p>
+			<?php
+				}
+			?>
+				<table class="table">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Name</th>
+							<th>Rating</th>
+							<th>Date</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+							$sql = "SELECT * FROM `movies`";
+							$result = mysqli_query($mysqli, $sql);
+							while($row = mysqli_fetch_assoc($result)){
+						?>
+							<tr>
+								<td><?php echo $row['id'] ?><br></td>
+								<td><?php echo $row['name'] ?><br></td>
+								<td><?php echo $row['rating'] ?><br></td>
+								<td><?php echo $row['reg_date'] ?><br></td>
+							</tr>
+						<?php
+							}							
+						?>
+					</tbody>
+				</table>
+			</div>
+			<hr>
+			<a href="index.php" class="btn btn-primary">Back</a>
+		</div>
+	</div>
+</body>
+</html>
